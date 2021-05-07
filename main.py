0670c50a18c1431e2168ae3413ddf5b258c06cf9
SbongiseniMbaraga <sbongiseni2@hotmail.com>
2021-05-07 19:29:40 +0200

    updated

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ response = requests.get("https://news.ycombinator.com/")
 yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
-article_tag = soup.find(name="a", class_="storylink")
-article_text = article_tag.text
-article_link = article_tag.get("href")
-article_upvote = soup.find(name="span", class_="score")
-article_upvote = article_upvote.getText()
+articles = soup.find_all(name="a", class_="storylink")
+article_texts = []
+article_links = []
+
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvote = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+print(article_texts)
+print(article_links)
+print(article_upvote)
 
 
 
